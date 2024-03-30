69c7cafa129965dd6645d3e9d8e79ac65c60ccfe
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-03-30 17:00:06 +0300

    added beautiful skeleton page

--- src/app/(site)/(dashboard)/layout.tsx
+++ src/app/(site)/(dashboard)/layout.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
-   <div className="fixed flex w-full justify-center">
-    <div className="sidebar flex-grow max-h-screen">
+   <div className="fixed flex w-full justify-center bg-transparent">
+    <div className="sidebar flex-grow max-h-screen bg-transparent">
       <DashboardSidebar/>
     </div>
-    <div className="p-0 m-0 w-full max-w-full h-screen overflow-y-scroll overflow-x-auto border-l-2 border-lime-200">
+    <div className="p-0 m-0 w-full max-w-full h-screen overflow-y-scroll overflow-x-auto border-l-2 border-lime-200 bg-white">
       {
         children
       }
--- src/app/(site)/(dashboard)/workspace/[id]/page.tsx
+++ src/app/(site)/(dashboard)/workspace/[id]/page.tsx
@@ -1,15 +1,18 @@
 'use client'
+import { UserPlusIcon } from '@heroicons/react/24/solid'
+import { Button, Typography } from '@material-tailwind/react'
 import { Workspace } from '@prisma/client'
 import { useQuery } from '@tanstack/react-query'
 import AvatarStack from 'base/app/components/AvatarStack'
-import { Breadcrumb } from 'base/app/components/BreadCrumbs'
+import { Breadcrumb, BreadcrumbFallback } from 'base/app/components/BreadCrumbs'
+import { ImageCard, ImageCardFallback } from 'base/app/components/ImageCard'
 import { WorkspaceTable } from 'base/app/components/workspace/WorkspaceTable'
 import { getDomain } from 'base/lib/utils'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-const page = () => {
+const Page = () => {
   const {id}:{id:string} = useParams();
   const {data:workspace,isLoading:workspaceLoading,error:workspaceError}= useQuery({
     queryFn:()=>fetch_workspace(),
@@ -48,7 +51,7 @@ const page = () => {
               </div>
               <div className="title-creator flex flex-col gap-2 w-[85%]">
                 <h1 className="text-3xl font-extrabold text-brown-900">{workspace?.title }</h1>
-                <p className=" text-brown-900 text-sm font-sans">{workspace?.description}{workspace?.description}{workspace?.description}{workspace?.description}{workspace?.description}{workspace?.description}{workspace?.description}</p>
+                <p className=" text-brown-900 text-sm font-sans">{workspace?.description}</p>
                 <AvatarStack/>
               </div>
             </div>
@@ -66,28 +69,108 @@ const page = () => {
             }</pre>
             "string"
           </section> */}
-        <Breadcrumb/>        
+        <Breadcrumb/> 
+
+
+        <section className="recent-activities px-6 py-6">
+            <div className="mb-8 flex items-center justify-between gap-8">
+                <div>
+                  <Typography variant="h5" color="blue-gray">
+                    Recent Activities
+                  </Typography>
+                  <Typography color="gray" className="mt-1 font-normal">
+                    See information about all members
+                  </Typography>
+                </div>
+                <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                  <Button variant="outlined" size="sm">
+                    view all
+                  </Button>
+                  <Button className="flex items-center gap-3" size="sm">
+                    <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add member
+                  </Button>
+                </div>
+            </div>
+
+            <div className="activity-list grid grid-cols-3 gap-3 justify-evenly w-full">
+            {
+              [1,2,3,4,5].map((num,index)=>(
+                <ImageCard/>
+              ))
+            }
+            </div>
+
+        </section>
+        <section className="diagrams px-6 py-6">
+            <div className="mb-8 flex items-center justify-between gap-8">
+                <div>
+                  <Typography variant="h5" color="blue-gray">
+                    Flow Diagrams
+                  </Typography>
+                  <Typography color="gray" className="mt-1 font-normal">
+                    See information about all members
+                  </Typography>
+                </div>
+                <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                  <Button variant="outlined" size="sm">
+                    view all
+                  </Button>
+                  <Button className="flex items-center gap-3" size="sm">
+                    <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add member
+                  </Button>
+                </div>
+            </div>
+              {/* header with title and action buttons*/}
+            <div className="mb-8 flex items-center justify-between gap-8 p-6">
+                <div>
+                  <p className="w-[20%] h-3 bg-[#c5c5c5] rounded-[10px] animate-pulse" />
+                  <Typography color="gray" className="mt-1 font-normal">
+                  c5c5c5   See information about all members
+                  </Typography>
+                </div>
+                <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                  <Button variant="outlined" size="sm">
+                    view all
+                  </Button>
+                  <Button className="flex items-center gap-3" size="sm">
+                    <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add member
+                  </Button>
+                </div>
+              </div>
+
+            <div className="activity-list grid grid-cols-3 gap-3 justify-evenly w-full">
+            {
+              [1,2,3,4,5].map((num,index)=>(
+                <ImageCard/>
+              ))
+            }
+            </div>
+
+        </section>
+
+
+            
         <WorkspaceTable/>
     </div>
     // </Suspense>
   )
 }
 
-export default page
+export default Page
 
 
 
 
 function WorkspaceFallback(){
 return (
-  <div className='overflow-y-scroll  h-fit bg-white '>
+  <div className='overflow-y-scroll  h-fit bg-white  '>
   <div className=' flex justify-between items-start w-full min-h-[10rem]  p-10 bg-[#d2d2d274]'>
     <div className="workspace-detail flex flex-row gap-4 items-center w-full">
       <div className=" w-28 h-28  rounded-[15px]  shadow-2xl bg-[#8a888832] shadow-[#28282887] border-[#e7f0e6] overflow-hidden">
           
       </div>
       <div className="title-creator flex flex-col gap-4 w-[85%]">
-        <div className=" text-3xl font-extrabold bg-g w-[40%] h-6 bg-[#8a888832] rounded-xl"/>
+        <div className="animate-pulse text-3xl font-extrabold bg-g w-[40%] h-6 bg-[#8a88883c] rounded-xl"/>
 
           <div className="description flex flex-col gap-2 w-full">
             <div className="animate-pulse text-3xl font-extrabold bg-g w-[90%] h-4 bg-[#8a888832] rounded-xl"/>
@@ -102,6 +185,32 @@ return (
     </div>
    
   </div>
+  <BreadcrumbFallback/>        
+  
+  <section className='py-6'>
+     {/* header with title and action buttons*/}
+            {/* header started here  */}
+            <div className="mb-8 flex items-center justify-between gap-8 px-6 ">
+            <div className='flex flex-col gap-2 w-full'>
+              <p className="w-[20%] h-4 bg-[#c5c5c5] rounded-[10px] animate-pulse" />
+              <p className="w-[45%] h-3 bg-[#c5c5c5] rounded-[10px] animate-pulse" />                    
+            </div>
+            <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+              <button className='w-12 h-5 bg-[#c5c5c5] rounded-[10px] animate-pulse'/>
+              <button className='w-12 h-5 bg-[#c5c5c5] rounded-[10px] animate-pulse'/>
+
+              
+            </div>
+          </div>
+        {/* header ends here  */}
+        <div className="activity-list grid grid-cols-3 gap-3 justify-evenly w-full px-6 py-2">
+            {
+              [1,2,3,4,5].map((num,index)=>(
+                <ImageCardFallback/>
+              ))
+            }
+        </div>
+  </section>
  
 </div>
 )
