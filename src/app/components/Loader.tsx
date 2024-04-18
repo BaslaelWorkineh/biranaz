f7286287da66679d46d8da72b004d975dba1b32c
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-04-18 20:53:37 +0300

    some loader ui modification

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -1,7 +1,11 @@
 
 const Loader = ({isVisible}:{isVisible:boolean}) => {
   return (
-    isVisible && <div className="absolute w-full h-full flex justify-center items-center text-3xl font-light font-sans text-brown-800 animate-pulse backdrop-blur-sm">Loading ...</div>
+    isVisible && <div className="absolute w-full h-full flex justify-center items-center text-3xl font-light font-sans text-brown-800  backdrop-blur-sm z-40 bg-[#ffcf9f44]">
+      <div className="animate-pulse">
+            Loading ...
+      </div>
+    </div>
   )
 }
 
